d34786d283d838c1f10632bebd5a64bc7fe1ddae
lys <ookm1020@gmail.com>
2019-11-12 16:19:54 +0900

    kakao map marker test

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -10,13 +10,28 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
-    var container = window.document.getElementById("map");
+    var mapContainer = window.document.getElementById("map");
+
     var options = {
       center: new window.kakao.maps.LatLng(33.450701, 126.570667),
       level: 3
     };
 
-    new window.kakao.maps.Map(container, options);
+    var map = new window.kakao.maps.Map(mapContainer, options);
+
+    // 마커가 표시될 위치입니다
+    var markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+
+    // 마커를 생성합니다
+    var marker = new window.kakao.maps.Marker({
+      position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+    // marker.setMap(null);
   }
 
   render() {
